f24af9a7a76794bd558530420d81ca1f22543efd
Stallion77RepoOfficial <149940678+Stallion77RepoOfficial@users.noreply.github.com>
2026-09-16 14:28:45 +0300

    Audit and optimize engine/src/movepick.h: noexcept, hoist invariants, skip quiets generation, const-correctness

--- engine/src/movepick.h
+++ engine/src/movepick.h
@@ -11,20 +11,19 @@ constexpr uint8_t BadCaptures = 5;
 }
 
 struct MovePicker {
-  int see_threshold;
-  int stage;
-  uint64_t checkers;
+  int see_threshold = 0;
+  int stage = Stages::TT;
+  uint64_t checkers = 0;
   int idx = 0;
 
-  MoveInfo captures;
-  MoveInfo quiets;
-  MoveInfo bad_captures;
-  StateRecord *ss;
+  MoveInfo captures{};
+  MoveInfo quiets{};
+  MoveInfo bad_captures{};
+  StateRecord *ss = nullptr;
 };
 
-inline void init_picker(MovePicker &picker, const Position &position, int threshold,
-                        uint64_t checkers, StateRecord *ss) {
-  (void)position;
+inline void init_picker(MovePicker &picker, [[maybe_unused]] const Position &position,
+                        int threshold, uint64_t checkers, StateRecord *ss) noexcept {
   picker.see_threshold = threshold;
   picker.stage = Stages::TT;
   picker.checkers = checkers;
@@ -36,7 +35,7 @@ inline void init_picker(MovePicker &picker, const Position &position, int thresh
 }
 
 inline Action next_move(MovePicker &picker, const Position &position,
-                        ThreadInfo &thread_info, Action tt_move, bool skip_quiets) {
+                        ThreadInfo &thread_info, Action tt_move, bool skip_quiets) noexcept {
 
   if (picker.stage == Stages::TT) {
     picker.stage++;
@@ -47,22 +46,21 @@ inline Action next_move(MovePicker &picker, const Position &position,
   }
 
   if (picker.stage == Stages::GenCaptures) {
-
     picker.captures.len = movegen(position, picker.captures.moves.data(),
                                   picker.checkers, Generate::GenCaptures);
 
     for (int i = 0; i < picker.captures.len; i++) {
-      Action move = picker.captures.moves[i];
+      const Action move = picker.captures.moves[i];
 
-      int from = extract_from(move), to = extract_to(move);
+      const int from = extract_from(move), to = extract_to(move);
       if (!is_valid_square(from) || !is_valid_square(to))
         continue;
 
+      const int from_piece = position.board[from];
       if (extract_type(move) == MoveTypes::Promotion) {
         if (extract_promo(move) == Promos::Queen) {
           picker.captures.scores[i] = QueenPromoScore;
         } else {
-          int from_piece = position.board[from];
           picker.captures.scores[i] =
               GoodCaptureBaseScore + evaluate_promotion_tactics(position, move);
           if (from_piece >= Pieces::WPawn && from_piece <= Pieces::BKing) {
@@ -71,10 +69,9 @@ inline Action next_move(MovePicker &picker, const Position &position,
           }
         }
       } else {
-        int from_piece = position.board[from];
-        int to_piece = (extract_type(move) == MoveTypes::EnPassant)
-                           ? (position.color == Colors::White ? Pieces::BPawn : Pieces::WPawn)
-                           : position.board[to];
+        const int to_piece = (extract_type(move) == MoveTypes::EnPassant)
+                                 ? (position.color == Colors::White ? Pieces::BPawn : Pieces::WPawn)
+                                 : position.board[to];
         picker.captures.scores[i] = GoodCaptureBaseScore +
                                     SeeValues[get_piece_type(to_piece)] * 100 -
                                     SeeValues[get_piece_type(from_piece)] / 100;
@@ -86,11 +83,10 @@ inline Action next_move(MovePicker &picker, const Position &position,
   }
 
   if (picker.stage == Stages::Captures) {
-
     while (picker.idx < picker.captures.len) {
-      Action move = get_next_move(picker.captures.moves.data(),
-                                  picker.captures.scores.data(), picker.idx++,
-                                  picker.captures.len);
+      const Action move = get_next_move(picker.captures.moves.data(),
+                                        picker.captures.scores.data(), picker.idx++,
+                                        picker.captures.len);
       if (move == tt_move) continue;
       if (SEE(position, move, picker.see_threshold)) {
         return move;
@@ -103,97 +99,86 @@ inline Action next_move(MovePicker &picker, const Position &position,
   }
 
   if (picker.stage == Stages::GenQuiets) {
-    picker.quiets.len = movegen(position, picker.quiets.moves.data(),
-                                picker.checkers, Generate::GenQuiets);
+    if (skip_quiets) {
+      picker.idx = 0;
+      picker.stage = Stages::BadCaptures;
+    } else {
+      picker.quiets.len = movegen(position, picker.quiets.moves.data(),
+                                  picker.checkers, Generate::GenQuiets);
 
-    int their_last = SquareNone;
-    int their_piece = Pieces::Blank;
+      int their_last = SquareNone;
+      int their_piece = Pieces::Blank;
+      int our_last = SquareNone;
+      int our_piece = Pieces::Blank;
+      int ply4last = SquareNone;
+      int ply4piece = Pieces::Blank;
 
-    int our_last = SquareNone;
-    int our_piece = Pieces::Blank;
-
-    int ply4last = SquareNone;
-    int ply4piece = Pieces::Blank;
-
-    if (thread_info.game_ply >= 1) {
-      int idx = thread_info.game_ply - 1;
-      if (idx >= 0 && idx < MaxGameLen) {
-        const StateRecord &h1 = thread_info.game_hist[idx];
-        if (h1.played_move != MoveNone) {
-          their_last = extract_to(h1.played_move);
-          their_piece = h1.piece_moved;
+      auto get_hist_move = [&](int offset, int &last_sq, int &piece) noexcept {
+        if (thread_info.game_ply >= offset && (thread_info.game_ply - offset) < MaxGameLen) {
+          const StateRecord &h = thread_info.game_hist[thread_info.game_ply - offset];
+          if (h.played_move != MoveNone) {
+            last_sq = extract_to(h.played_move);
+            piece = h.piece_moved;
+          }
         }
-      }
-    }
-    if (thread_info.game_ply >= 2) {
-      int idx = thread_info.game_ply - 2;
-      if (idx >= 0 && idx < MaxGameLen) {
-        const StateRecord &h2 = thread_info.game_hist[idx];
-        if (h2.played_move != MoveNone) {
-          our_last = extract_to(h2.played_move);
-          our_piece = h2.piece_moved;
-        }
-      }
-    }
-    if (thread_info.game_ply >= 4) {
-      int idx = thread_info.game_ply - 4;
-      if (idx >= 0 && idx < MaxGameLen) {
-        const StateRecord &h4 = thread_info.game_hist[idx];
-        if (h4.played_move != MoveNone) {
-          ply4last = extract_to(h4.played_move);
-          ply4piece = h4.piece_moved;
-        }
-      }
-    }
+      };
 
-    for (int i = 0; i < picker.quiets.len; i++) {
-      Action move = picker.quiets.moves[i];
+      get_hist_move(1, their_last, their_piece);
+      get_hist_move(2, our_last, our_piece);
+      get_hist_move(4, ply4last, ply4piece);
 
-      int from = extract_from(move), to = extract_to(move);
-      if (!is_valid_square(from) || !is_valid_square(to))
-        continue;
+      const Action killer0 = (thread_info.search_ply < MaxSearchPly)
+                                 ? thread_info.KillerMoves[thread_info.search_ply][0]
+                                 : MoveNone;
+      const Action killer1 = (thread_info.search_ply < MaxSearchPly)
+                                 ? thread_info.KillerMoves[thread_info.search_ply][1]
+                                 : MoveNone;
+      const Action counter_move = (their_piece != Pieces::Blank && their_last != SquareNone)
+                                      ? thread_info.CounterMoves[their_piece][their_last]
+                                      : MoveNone;
 
-      if (move == thread_info.KillerMoves[thread_info.search_ply][0]) {
-        picker.quiets.scores[i] = KillerMoveScore;
-      } else if (move == thread_info.KillerMoves[thread_info.search_ply][1]) {
-        picker.quiets.scores[i] =
-            KillerMoveScore - 1000;
-      }
+      for (int i = 0; i < picker.quiets.len; i++) {
+        const Action move = picker.quiets.moves[i];
 
-      else {
+        const int from = extract_from(move), to = extract_to(move);
+        if (!is_valid_square(from) || !is_valid_square(to))
+          continue;
 
-        int piece = position.board[from];
-        picker.quiets.scores[i] = thread_info.HistoryScores[piece][to];
+        if (move == killer0) {
+          picker.quiets.scores[i] = KillerMoveScore;
+        } else if (move == killer1) {
+          picker.quiets.scores[i] = KillerMoveScore - 1000;
+        } else {
+          const int piece = position.board[from];
+          picker.quiets.scores[i] = thread_info.HistoryScores[piece][to];
 
-        if (their_last != SquareNone) {
-          picker.quiets.scores[i] +=
-              thread_info.ContHistScores[their_piece][their_last][piece][to];
-        }
-        if (our_last != SquareNone) {
-          picker.quiets.scores[i] +=
-              thread_info.ContHistScores[our_piece][our_last][piece][to];
-        }
+          if (their_last != SquareNone) {
+            picker.quiets.scores[i] +=
+                thread_info.ContHistScores[their_piece][their_last][piece][to];
+          }
+          if (our_last != SquareNone) {
+            picker.quiets.scores[i] +=
+                thread_info.ContHistScores[our_piece][our_last][piece][to];
+          }
+          if (ply4last != SquareNone) {
+            picker.quiets.scores[i] +=
+                thread_info.ContHistScores[ply4piece][ply4last][piece][to];
+          }
 
-        if (ply4last != SquareNone) {
-          picker.quiets.scores[i] +=
-              thread_info.ContHistScores[ply4piece][ply4last][piece][to];
-        }
-
-        if (their_piece != Pieces::Blank && their_last != SquareNone) {
-          Action counter = thread_info.CounterMoves[their_piece][their_last];
-          if (move == counter)
+          if (counter_move != MoveNone && move == counter_move) {
             picker.quiets.scores[i] += 8000;
+          }
         }
       }
+      picker.stage++;
     }
-    picker.stage++;
   }
 
   if (picker.stage == Stages::Quiets) {
     while (!skip_quiets && picker.idx < picker.quiets.len) {
-      Action move = get_next_move(picker.quiets.moves.data(),
-                                  picker.quiets.scores.data(), picker.idx++,
-                                  picker.quiets.len);
+      const Action move = get_next_move(picker.quiets.moves.data(),
+                                        picker.quiets.scores.data(), picker.idx++,
+                                        picker.quiets.len);
       if (move != tt_move) return move;
     }
     if (skip_quiets || picker.idx >= picker.quiets.len) {
